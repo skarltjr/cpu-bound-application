2dc66467633acff148570ec95710e26c186ce75b
skarltjr <kisa0828@naver.com>
2021-02-08 18:43:22 +0900

    chage get helllo

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello kiseok2";
+        return "hello kiseok3";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
